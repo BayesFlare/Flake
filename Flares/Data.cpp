4cf87b8f8c4c55a602145999f558f0220ff6d5b0
Matthew Pitkin <matthew.pitkin@ligo.org>
2015-02-16 16:29:51 +0000

    Changed Data.cpp class to read in Kepler data from a fits file using the CCfits library

--- Flares/Data.cpp
+++ Flares/Data.cpp
@@ -1,8 +1,8 @@
 #include "Data.h"
-#include <fstream>
-#include <iostream>
 
-using namespace std;
+#include <CCfits/CCfits>
+
+using namespace CCfits;
 
 Data Data::instance;
 
@@ -11,27 +11,49 @@ Data::Data()
 
 }
 
+// A function for reading in Kepler light curve data from a FITS file.
+// This uses the C++ CFITSIO interface CCfits. 
+// See http://heasarc.gsfc.nasa.gov/docs/software/fitsio/ccfits/html/readtable.html for an example
+
 void Data::load(const char* filename)
 {
-	fstream fin(filename, ios::in);
-	if(!fin)
-	{
-		cerr<<"# Error. Couldn't open file "<<filename<<"."<<endl;
-		return;
-	}
-
-	// Empty the vectors
-	t.clear();
-	y.clear();
-
-	double temp1, temp2;
-	while(fin>>temp1 && fin>>temp2)
-	{
-		t.push_back(temp1);
-		y.push_back(temp2);
-	}
-	cout<<"# Loaded "<<t.size()<<" data points from file "
-			<<filename<<"."<<endl;
-	fin.close();
+  // use the verbose mode to output any errors in reading files
+  FITS::setVerboseMode(true);
+  
+  // Empty the vectors
+  t.clear(); // the light curve time stamps in seconds
+  y.clear(); // the light curve flux
+  
+  // set the names of the Kepler FITS file HDUs
+  std::vector<string> hdus(3);
+  hdus[0] = "PRIMARY";
+  hdus[1] = "LIGHTCURVE"; // contains the light curve data
+  hdus[2] = "APERTURE";
+  
+  // create the FITS class (false means that all data is *not* read on construction)
+  std::auto_ptr<FITS> pInfile(new FITS(filename, Read, hdus, false));
+  
+  // create a table for the LIGHTCURVE HDU 
+  ExtHDU& table = pInfile->extension(hdus[1]);
+  
+  long nrows = table.rows(); // number of values in the light curve 
+  
+  // read in the times (in days in the light curve file) and convert to seconds (from first time stamp)
+  table.column("TIME").read( t, 1, nrows ); // read from first row (1) to last
+  for ( std::vector<double>::iterator i=t.begin(); i != t.end(); ++i ) { *i = (*i-t[0])*86400.; }
+  
+  // read in the light curve data from the PDCSAP_FLUX data channel
+  //  - there are other channels available, see http://archive.stsci.edu/kepler/manuals/archive_manual.pdf
+  table.column("PDCSAP_FLUX").read( y, 1, nrows );
+  
+  // loop through light curve and remove any NaN or inf entries
+  int j = 0, size = y.size();
+  for ( int i=0; i < size; i++ ){
+    if ( std::isnan(y[j]) || std::isinf(y[j]) ){
+      y.erase(y.begin()+j);
+      t.erase(t.begin()+j);
+    }
+    else{ j++; }
+  }
 }
 
--- Flares/main.cpp
+++ Flares/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Start.h"
+#include <Start.h>
 #include "MyModel.h"
 #include "Data.h"
 
@@ -8,12 +8,17 @@ using namespace DNest3;
 
 int main(int argc, char** argv)
 {
-	// Load the data
-	Data::get_instance().load("fake_data.txt");
+  // Load the data
+  CommandLineOptions options(argc, argv);
+  string dataFile = options.get_dataFile();
+  if(dataFile.compare("") == 0)
+    cerr << "# ERROR: Kepler FITS filename required with -d argument." <<endl;
+  else
+    Data::get_instance().load(dataFile.c_str());
+        
+  MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
+  sampler.run();
 
-	MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
-	sampler.run();
-
-	return 0;
+  return 0;
 }
 
