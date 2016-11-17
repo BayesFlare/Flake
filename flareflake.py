5ca4672480da7c7a148888c138b44c0f9c4afba1
Aaron Peat <aaronpeat@hotmail.com>
2016-11-17 23:49:57 +0000

    flareflake.py runs FlareGenerator.py and feeds its output into Flake. Every 60 seconds it runs postprocess on Flake's output to check the natural logarithm of the marginal likelihood (log(Z)). If log(Z) is above a threshold (currently, 1, therefore, perfect, this will be changed in future), it will kill flake and end. A keyboard interrupt will also kill flake and end the program.

---
+++ flareflake.py
@@ -0,0 +1,51 @@
+import os.path as path
+import subprocess
+import time
+import sys
+import numpy as np
+
+sys.path.insert(0, './src/')
+
+import postprocess1 as pps
+
+
+cleanup=True
+
+print("Running FlareGenerator.py\n")
+subprocess.call(["python3", "./FlareGenerator.py"])
+filen=open('./filename.txt') #file is a command, so I couldn't call the variable file
+filename=filen.read()
+filen.close()
+subprocess.call(["rm", "filename.txt"])
+flake_process=subprocess.Popen(["./Flake-master/flake", "-d", filename])
+
+
+
+checktime=60 #How often postprocess is run
+sleeptime=10 #How often the system should report the time remaining
+             #Ideally this would be a factor of $checktime
+threshold=1  #1 will never be reached, here for now
+
+             
+end = False
+while end is False:
+    try:
+        a=0
+        print(checktime, "seconds remaining until next postprocess run")
+        while a<checktime:
+            time.sleep(sleeptime)
+            a=a+sleeptime
+            print(checktime-a, "seconds remaining until next postprocess run")
+        logz_estimate, H_estimate, logx_samples=pps.postprocess(save=False)
+
+        if logz_estimate>threshold:
+            end = True
+            print("Threshold reached. Killing Flake.")
+
+
+    except (KeyboardInterrupt):
+        break
+        print("Keyboard Interrupt. Killing Flake.")
+
+
+flake_process.kill()
