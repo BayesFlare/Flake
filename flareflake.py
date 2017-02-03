f0a8321a59ca8042713f1256cb1d2dd372842e39
Aaron Peat <aaronpeat@hotmail.com>
2017-02-03 23:06:26 +0000

    Minor bug fixes and tweaks

--- flareflake.py
+++ flareflake.py
@@ -93,77 +93,15 @@ if mode==2:
         filename[0]=txtlist[0]
         print("Found and analysing", filename)
 
-print("\nRunning first Flake run of two.")
-
-
 
 for u in range(0, len(filename)):
 
     flake_process=subprocess.Popen(["./Flake-master/flake", "-d", filename[u], "-f", "./flake_settings.json"])
 
-    checktime=60 #How often postprocess is run
-    sleeptime=10 #How often the system should report the time remaining
-                 #Ideally this would be a factor of $checktime
     p_samples=0
-    end = False
-    while end is False:
-        try:
-            if p_samples==1:
-                print("\nPostprocess error ecountered. Rerunning postprocess in 10 seconds.\n")
-                a=checktime-10
-            else:
-                a=0
-                print("\n"+str(checktime-a), "seconds remaining until next postprocess run. (First Flake run of two)\n")
-            while a<checktime:
-                time.sleep(sleeptime)
-                a=a+sleeptime
-                if checktime-a>0:
-                    print("\n"+str(checktime-a), "seconds remaining until next postprocess run. (First Flake run of two)\n")
-                else:
-                    print("\nInitiating postprocess run...\n")
-                    p_samples=pps.postprocess(save=False, plot=False)
-            if p_samples!=1:
-                if np.max(p_samples[-10:]) < 1.0e-5:
-                    end = True
-                    print("\nThreshold consistently reached. Killing Flake.\nFor reference maximum of last 10 p_samples=", str(np.max(p_samples[-10:])))
-                else:
-                    print("\nThreshold not consistently reached, maximum of last 10 p_samples=", str(np.max(p_samples[-10:]))+". Less than 10^-5 required.\nContinuing Flake run.\n")
-
-        except(KeyboardInterrupt):
-            print(" Keyboard Interrupt. Killing Flake.")
-            break
-
-    flake_process.kill()
-
-    #Rewriting Options
-
-    samples=np.loadtxt("./sample.txt")
-    nlevels=len(samples)
-
-    options=open("./OPTIONS", "r")
-    options_data=options.readlines()
-    options.close()
-
-    options_data[6]=str(nlevels)+"\t# maximum number of levels\n"
-    print("Rewriting OPTIONS file.")
-    options_tmp=open("./OPTIONS.tmp", 'w')
-
-    for i in options_data:
-        options_tmp.write(i)
-    options_tmp.close()
-
-    shutil.move('./OPTIONS.tmp', './OPTIONS')
-
-    options=open("./OPTIONS", "r")
-    options_data_new=options.readlines()
-    for i in range(0, len(options_data)):
-        if options_data_new[i]!=options_data[i]:
-            print("Critical Failure: OPTIONS file not succesfully rewritten.")
-            exit()
-    print("OPTIONS file succesfully rewritten!")
 
     #Running Flake again
-    print("\nInitiating second Flake run of two.")
+    print("\nInitiating Flake run.")
     flake_process=subprocess.Popen(["./Flake-master/flake", "-d", filename[u], "-f", "flake_settings.json"])
 
     checktime=120 #How often postprocess is run
@@ -179,18 +117,18 @@ for u in range(0, len(filename)):
                 a=checktime-10
             else:
                 a=0
-            print("\n"+str(checktime-a), "seconds remaining until next postprocess run. (Second Flake run of two)\n")
+            print("\n"+str(checktime-a), "seconds remaining until next postprocess run.\n")
             while a<checktime:
                 time.sleep(sleeptime)
                 a=a+sleeptime
                 if checktime-a>0:
-                    print("\n"+str(checktime-a), "seconds remaining until next postprocess run. (Second Flake run of two)\n")
+                    print("\n"+str(checktime-a), "seconds remaining until next postprocess run.\n")
                 else:
                     print("\nInitiating postprocess run...\n")
                     p_samples=pps.postprocess(save=False, plot=False, save_posterior=True)
                     posterior=np.loadtxt('./posterior_sample.txt')
             if p_samples!=1:   
-                if len(posterior)>=n_posterior_samples and len(np.shape(posterior))>1:
+                if len(posterior)>=n_posterior_samples and len(np.shape(posterior))>1 and np.max(p_samples[-10:]) < 1.0e-5:
                     end = True
                     print("\nAt least "+str(n_posterior_samples)+" samples acquired ("+str(len(posterior))+"). Killing Flake.\n")
                 else:
